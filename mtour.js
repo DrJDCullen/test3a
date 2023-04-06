2e6588be8f1f9be943c5adb02701c2daf078dd06
Cullen <GNRJCULL@ljmu.ac.uk>
2023-04-06 13:53:42 +0100

    Initial upload

---
+++ mtour.js
@@ -0,0 +1,124 @@
+var APP_DATA = {
+    "initialscene": "Corridor",
+    "name": "Sample Tour 3",
+    "scenes": [
+        {
+            "faceSize": 2688,
+            "id": "Corridor",
+            "infoHotspots": [
+                {
+                    "icon": "pmicons/winfo.png",
+                    "pitch": -0.024556782578766666,
+                    "rotation": 0,
+                    "text": "To the rest of Henry Cotton First Floor",
+                    "title": "HC1st Floor",
+                    "yaw": -2.900719763900811
+                }
+            ],
+            "initialViewParameters": {
+                "fov": 1.2,
+                "pitch": 0,
+                "yaw": 0
+            },
+            "levels": [
+                {
+                    "size": 256,
+                    "tileSize": 256
+                },
+                {
+                    "size": 256,
+                    "tileSize": 256
+                },
+                {
+                    "size": 512,
+                    "tileSize": 256
+                },
+                {
+                    "size": 1024,
+                    "tileSize": 256
+                },
+                {
+                    "size": 2048,
+                    "tileSize": 256
+                }
+            ],
+            "linkHotspots": [
+                {
+                    "icon": "pmicons/wlink000.png",
+                    "initialViewParameters": {
+                        "fov": 1.2,
+                        "pitch": 0,
+                        "yaw": 0
+                    },
+                    "options": {
+                        "transitionDuration": 2000
+                    },
+                    "pitch": 0.024539329286244447,
+                    "rotation": 0,
+                    "target": "HC133",
+                    "title": "HCLab",
+                    "yaw": 0.1484402529015
+                }
+            ],
+            "name": "Corridor"
+        },
+        {
+            "faceSize": 2688,
+            "id": "HC133",
+            "infoHotspots": [
+            ],
+            "initialViewParameters": {
+                "fov": 1.2,
+                "pitch": 0,
+                "yaw": 0
+            },
+            "levels": [
+                {
+                    "size": 256,
+                    "tileSize": 256
+                },
+                {
+                    "size": 256,
+                    "tileSize": 256
+                },
+                {
+                    "size": 512,
+                    "tileSize": 256
+                },
+                {
+                    "size": 1024,
+                    "tileSize": 256
+                },
+                {
+                    "size": 2048,
+                    "tileSize": 256
+                }
+            ],
+            "linkHotspots": [
+                {
+                    "icon": "pmicons/wlink000.png",
+                    "initialViewParameters": {
+                        "fov": 1.2,
+                        "pitch": 0,
+                        "yaw": 0
+                    },
+                    "options": {
+                        "transitionDuration": 2000
+                    },
+                    "pitch": 0.049078658572488894,
+                    "rotation": 0,
+                    "target": "Corridor",
+                    "title": "Back to Corridor",
+                    "yaw": -3.0856024915887112
+                }
+            ],
+            "name": "HC133"
+        }
+    ],
+    "settings": {
+        "autorotateEnabled": false,
+        "fullscreenButton": true,
+        "mouseViewMode": "drag",
+        "viewControlButtons": true
+    }
+}
